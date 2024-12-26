ee8654a10a51ee9fdfeb7214fa5d7b11bd7f2b21
SabaMarghania1 <marghanias@gmail.com>
2024-12-26 18:30:19 +0400

    setup prisma

--- beststream-backend/src/main.ts
+++ beststream-backend/src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { CoreModule } from './core/сore.module'
+import { NestFactory } from '@nestjs/core'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(CoreModule)
+	await app.listen(3000)
 }
-bootstrap();
+bootstrap()
